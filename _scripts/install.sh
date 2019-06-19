2de40809767313d1e2d2224f9e0112f482f987ff
Longwei <longwei66@gmail.Com>
2019-06-19 20:06:24 +0200

    fix the install scrip on ssh key management

---
+++ _scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x # Show the output of the following commands (useful for debugging)
+    
+# Import the SSH deployment key
+openssl aes-256-cbc -K $encrypted_04551ded9036_key -iv $encrypted_04551ded9036_iv -in deploy-key.enc -out deploy-key -d
+
+rm deploy-key.enc # Don't need it anymore
+chmod 600 deploy-key
+mv deploy-key ~/.ssh/id_rsa
