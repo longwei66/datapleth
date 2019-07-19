bce04b5bf6c2b4ed396ff15cb69b9f1f6d6e917a
Longwei <longwei66@gmail.Com>
2019-07-19 21:19:09 +0200

    remove showtext package and add install of chinese fonts

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -7,3 +7,7 @@ openssl aes-256-cbc -K $encrypted_04551ded9036_key -iv $encrypted_04551ded9036_i
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
+
+# Add necessary packages
+apt-get install fonts-wqy-zenhei
+apt-get install fonts-arphic-bkai00mp fonts-arphic-bsmi00lp fonts-arphic-gbsn00lp fonts-arphic-gkai00mp fonts-arphic-ukai fonts-arphic-uming fonts-cns11643-kai fonts-cns11643-sung fonts-cwtex-fs fonts-cwtex-heib fonts-cwtex-kai fonts-cwtex-ming fonts-cwtex-yen
