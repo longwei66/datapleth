767bb8f0256994c76a2d3cf554dd29fb41fb602b
Longwei <longwei66@gmail.Com>
2019-07-20 14:11:23 +0200

    test with bionic instead of xenial and put packages install in main yml, attempt to install java and rJava

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
-    
+
+# for rJava
+sudo R CMD javareconf
+
+
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_04551ded9036_key -iv $encrypted_04551ded9036_iv -in deploy-key.enc -out deploy-key -d
 
@@ -9,5 +13,5 @@ chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
 
 # Add necessary packages
-sudo apt-get install fonts-wqy-zenhei
-sudo apt-get install fonts-arphic-bkai00mp fonts-arphic-bsmi00lp fonts-arphic-gbsn00lp fonts-arphic-gkai00mp fonts-arphic-ukai fonts-arphic-uming fonts-cns11643-kai fonts-cns11643-sung fonts-cwtex-fs fonts-cwtex-heib fonts-cwtex-kai fonts-cwtex-ming fonts-cwtex-yen
+#sudo apt-get install fonts-wqy-zenhei
+#sudo apt-get install fonts-arphic-bkai00mp fonts-arphic-bsmi00lp fonts-arphic-gbsn00lp fonts-arphic-gkai00mp fonts-arphic-ukai fonts-arphic-uming fonts-cns11643-kai fonts-cns11643-sung fonts-cwtex-fs fonts-cwtex-heib fonts-cwtex-kai fonts-cwtex-ming fonts-cwtex-yen
