582b5f0aae6394e1591c5434d5d0e6e5c32cb352
Longwei <longwei66@gmail.Com>
2019-12-03 16:36:54 +0100

    the previous attempt faile and broke production (that's bad) as for some reason the deployment does not work. I think it's because of caching or something like that, there are no html files in the public folder and no index.html. In this version, I put deploy script together with teh jobs and second stage hoping that work. I modified as well the deploy script to push to dev following this documentation https://kjaer.io/travis/

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://kjaer.io/travis/
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
@@ -13,5 +14,20 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
     git commit -m "Deploy"
     git push --force deploy master
 else
-    echo "Not deploying, since this branch isn't master."
+    echo "Not deploying to master, since this branch isn't master."
+fi
+if [ $TRAVIS_BRANCH == 'dev' ] ; then
+    # Initialize a new git repo in _site, and push it to our server.
+    cd public
+    git init
+        
+    git remote add deploy "deploy@188.166.216.253:/var/www/datapleth.io.dev"
+    git config user.name "Travis CI"
+    git config user.email "longwei66+travisCI@gmail.com"
+    
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy dev
+else
+    echo "Not deploying, since this branch isn't master or dev"
 fi
