d7ac65893f7fcebd654c900f77b90d55f697b2f4
Longwei <longwei66@gmail.Com>
2019-12-04 10:29:11 +0100

    create a simple branch beta, with only one post to speedup tests on travis

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -29,5 +29,20 @@ if [ $TRAVIS_BRANCH == 'dev' ] ; then
     git commit -m "Deploy"
     git push --force deploy master
 else
-    echo "Not deploying, since this branch isn't master or dev"
+    echo "Not deploying, since this branch isn't dev"
+fi
+if [ $TRAVIS_BRANCH == 'beta' ] ; then
+    # Initialize a new git repo in _site, and push it to our server.
+    cd public
+    git init
+        
+    git remote add deploy "deploy@188.166.216.253:/var/www/datapleth.io.beta"
+    git config user.name "Travis CI"
+    git config user.email "longwei66+travisCI@gmail.com"
+    
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
+else
+    echo "Not deploying, since this branch isn't master or dev or beta"
 fi
